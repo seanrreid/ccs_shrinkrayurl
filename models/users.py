b8fb1c8cff9201ce206887c6e4ab10a4437dc5ac
Sean Reid <sean@torchcodelab.com>
2024-07-01 12:08:29 -0400

    Updated user model, and migrations

--- models/users.py
+++ models/users.py
@@ -1,7 +1,16 @@
 from sqlmodel import Field
+from pydantic import EmailStr
+
+import bcrypt
+
 from .base import Base
 
 class User(Base, table=True):
     __tablename__ = "users"
 
-    username: str = Field(sa_column_kwargs={"unique": True})
+    username: EmailStr = Field(nullable=False, sa_column_kwargs={"unique": True})
+    hashed_password: str = Field(nullable=False)
+
+    @staticmethod
+    def hash_password(password) -> str:
+        return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
