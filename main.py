88752334a2b049f82ac7b4809f687200c65b3ae9
Sean R Reid <sean@torchcodelab.com>
2024-07-01 08:50:59 -0400

    Added routes to get/add urls

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import uvicorn
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
-
 from sqlmodel import Session, select
+from db import get_session
+
+from models.urls import Url
+from models.users import User
 
 # Setup our origins...
 # ...for now it's just our local environments
@@ -29,6 +32,22 @@ app.add_middleware(
 @app.get("/")
 def root():
     return {"message": "Root Route"}
+
+
+@app.get('/urls', status_code=200)
+async def get_links(session: Session = Depends(get_session)):
+    statement = select(Url)
+    results = session.exec(statement).all()
+    return results
+
+@app.post('/urls/add', status_code=200)
+async def add_link(url_data: Url, session: Session = Depends(get_session)):
+    new_link = Url(**url_data.dict())
+    session.add(new_link)
+    session.commit()
+    session.refresh(new_link)
+    return {"Url added:": new_link.title}
+
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='localhost', port=8000, reload=True)
